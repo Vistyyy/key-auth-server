e2f135e67b79ac54616fb73c7d651350f3025781
Vistyyy <vistywastakentwo@gmail.com>
2025-05-03 11:10:21 +0300

    Update addtime.php

--- api/addtime.php
+++ api/addtime.php
@@ -1,1 +1,17 @@
+<?php
+$data = json_decode(file_get_contents('php://input'), true);
+$key = $data['key'];
+$minutes = intval($data['minutes']);
+$users = json_decode(file_get_contents("users.json"), true);
+
+foreach ($users as &$user) {
+    if ($user["key"] == $key) {
+        $user["expiry"] = max(time(), $user["expiry"]) + ($minutes * 60);
+        file_put_contents("users.json", json_encode($users, JSON_PRETTY_PRINT));
+        echo "Time added.";
+        exit;
+    }
+}
+echo "Key not found.";
+?>
 

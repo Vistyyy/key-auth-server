0ed5e55d2a9dbd398ad5e42c8ad109b7ef8487b6
Vistyyy <vistywastakentwo@gmail.com>
2025-05-03 11:09:46 +0300

    Update register.php

--- api/register.php
+++ api/register.php
@@ -1,1 +1,29 @@
+<?php
+$data = json_decode(file_get_contents('php://input'), true);
+$name = $data['name'];
+$key = $data['key'];
+$hwid = $data['hwid'];
+$duration = intval($data['minutes']);
+
+$users = json_decode(file_get_contents("users.json"), true);
+
+foreach ($users as $user) {
+    if ($user["key"] == $key) {
+        http_response_code(409);
+        echo "Key already registered.";
+        exit;
+    }
+}
+
+$expiry = time() + ($duration * 60);
+$users[] = [
+    "name" => $name,
+    "key" => $key,
+    "hwid" => $hwid,
+    "expiry" => $expiry
+];
+
+file_put_contents("users.json", json_encode($users, JSON_PRETTY_PRINT));
+echo "User registered.";
+?>
 

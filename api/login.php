917ac71ca55c08877676308324efe1c8e5781d0b
Vistyyy <vistywastakentwo@gmail.com>
2025-05-03 11:10:05 +0300

    Update login.php

--- api/login.php
+++ api/login.php
@@ -1,1 +1,25 @@
+<?php
+$data = json_decode(file_get_contents('php://input'), true);
+$key = $data['key'];
+$hwid = $data['hwid'];
+$users = json_decode(file_get_contents("users.json"), true);
+
+foreach ($users as $user) {
+    if ($user["key"] == $key) {
+        if ($user["hwid"] == $hwid) {
+            if (time() < $user["expiry"]) {
+                echo "VALID|" . $user["name"];
+                exit;
+            } else {
+                echo "EXPIRED";
+                exit;
+            }
+        } else {
+            echo "HWID_MISMATCH";
+            exit;
+        }
+    }
+}
+echo "KEY_NOT_FOUND";
+?>
 

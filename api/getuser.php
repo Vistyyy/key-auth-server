4c70f960a82e785f8399b280779b5c70f04aa0e1
Vistyyy <vistywastakentwo@gmail.com>
2025-05-03 11:10:36 +0300

    Update getuser.php

--- api/getuser.php
+++ api/getuser.php
@@ -1,1 +1,14 @@
+<?php
+$data = json_decode(file_get_contents('php://input'), true);
+$key = $data['key'];
+$users = json_decode(file_get_contents("users.json"), true);
+
+foreach ($users as $user) {
+    if ($user["key"] == $key) {
+        echo $user["name"];
+        exit;
+    }
+}
+echo "Not found";
+?>
 
